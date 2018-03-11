1dfe025095ad7ca54b0df93946f18f04d0f5cd01
GoForBroke <go.for.broke1006@gmail.com>
2018-03-11 18:47:00 +0300

    Fix unitily functions

--- std.h
+++ std.h
@@ -34,19 +34,11 @@ public:
     }
 };
 
-int coord2pos(int colsCount, int row, int col) {
-    return row * colsCount + col;
-}
-
-int myRand(int from, int to) {
-    return rand() % to + from;
-}
-
 class Rectangle {
 private:
     int x, y, w, h;
 public:
-    Rectangle() {}
+//    Rectangle() {}
 
     Rectangle(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
 
@@ -83,31 +75,42 @@ public:
     }
 };
 
-int get_row_pos(int pos, int colsCount) { return pos / colsCount + (pos % colsCount > 0 ? 1 : 0); }
+int coord2pos(int colsCount, int row, int col) {
+    return row * colsCount + col;
+}
 
-int get_col_pos(int pos, int colsCount) { return pos % colsCount; }
+int myRand(int from, int to) {
+    return rand() % to + from;
+}
+
+int get_row_pos(const int pos, const int colsCount) {
+    int lp = pos + 1;
+    return lp / colsCount + (lp % colsCount > 0 ? 1 : 0) - 1;
+}
+
+int get_col_pos(const int pos, const int colsCount) { return pos % colsCount; }
 
 Rectangle *get_rect(
-        int position,
-        int rowsCount, int colsCount,
-        int screenWidth, int screenHeght,
-        int margin
+        const int position,
+        const int rowsCount, const int colsCount,
+        const int screenWidth, const int screenHeght,
+        const int margin
 ) {
-    int cellWidth = screenWidth / colsCount;
-    int cellHeight = screenHeght / rowsCount;
+    const int cw = screenWidth / colsCount;
+    const int ch = screenHeght / rowsCount;
 
-    int
+    const int
             x = get_col_pos(position, colsCount),
             y = get_row_pos(position, colsCount);
 
-    int left = x * cellWidth + margin / 2;
-    int top = y * cellHeight + margin / 2;
+    int left = x * cw + margin / 2;
+    int top = y * ch + margin / 2;
 
     return new Rectangle(
             left,
             top,
-            cellWidth - margin,
-            cellHeight - margin
+            cw - margin,
+            ch - margin
     );
 }
 
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -9,8 +9,11 @@
 #include "../std.h"
 
 TEST_CASE("Position convertation works fine") {
-    REQUIRE(get_row_pos(101, 50) == 3);
-    REQUIRE(get_row_pos(99, 50) == 2);
+    REQUIRE(get_col_pos(5, 4) == 1);
+    REQUIRE(get_row_pos(5, 4) == 1);
+
+    REQUIRE(get_row_pos(101, 50) == 2);
+    REQUIRE(get_row_pos(99, 50) == 1);
     REQUIRE(get_col_pos(101, 50) == 1);
     REQUIRE(get_col_pos(19, 20) == 19);
     REQUIRE(get_row_pos(0, 50) == 0);
@@ -19,5 +22,14 @@ TEST_CASE("Position convertation works fine") {
 
 TEST_CASE("Getting rectangle by position works fine") {
     REQUIRE(get_rect(0, 2, 2, 100, 100, 2)->getW() == 48);
-    REQUIRE(get_rect(14, 4, 4, 100, 100, 2)->getX() == 76);
+
+    REQUIRE(get_rect(0, 4, 4, 100, 100, 2)->getX() == 1);
+    REQUIRE(get_rect(0, 4, 4, 100, 100, 2)->getY() == 1);
+
+    REQUIRE(get_rect(5, 4, 4, 100, 100, 2)->getX() == 26);
+    REQUIRE(get_rect(5, 4, 4, 100, 100, 2)->getY() == 26);
+
+//    REQUIRE(get_rect(14, 4, 4, 100, 100, 2)->getX() == 76);
+//    REQUIRE(get_rect(14, 4, 4, 100, 100, 2)->getX() == 76);
+//    REQUIRE(get_rect(14, 4, 4, 100, 100, 2)->getX() == 76);
 }
